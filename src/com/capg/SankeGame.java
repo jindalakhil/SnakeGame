2e1c0ae61122e2024d3f0cada073994030e83f45
jindalakhil <akhiljindal222@gmail.com>
2020-09-24 13:05:21 +0530

    [Akhil Jindal]Added: reaching 100 condition

--- src/com/capg/SankeGame.java
+++ src/com/capg/SankeGame.java
@@ -5,29 +5,34 @@ public class SankeGame {
 		System.out.println("Welcome to game");
 		int start = 0;
 		int position = start;
-		System.out.println(position);
 		
-//		while(position <= 100) {
-//			System.out.println(position);
-//			int diceValue = (int) Math.floor(Math.random() * 10) % 6;
-//			int action = (int) Math.floor(Math.random() * 10) % 3;
-//			int noPlay = 1;
-//			int ladder = 2;
-//			int snake = 0;
-//			if(action > 100) {
-//				System.out.println(action);
-//				continue;
-//			} else {
-//				if(action == noPlay) {
-//					System.out.println("Stay in the same position");
-//				}else if(action == ladder) {
-//					System.out.println("adding position");
-//					position += diceValue;
-//				} else if(action == snake){
-//					position -= diceValue;
-//				}
-//			}
-//			System.out.println(position);
-//		}
+		while(position != 100) {
+			int diceValue = (int) Math.floor(Math.random() * 10) % 6;
+			int action = (int) Math.floor(Math.random() * 10) % 3;
+			int noPlay = 1;
+			int ladder = 2;
+			int snake = 0;
+				if(action == noPlay) {
+					System.out.println("Stay in the same position");
+				}else if(action == ladder) {
+					int after = position+diceValue;
+					if(after >100) {
+						System.out.println("Stay in the same position");
+					} else {
+						position +=diceValue;
+					}
+					
+				} else if(action == snake){
+					int after = position-diceValue;
+					if(after <0) {
+						System.out.println("moving to zero");
+						position = 0;
+					} else {
+						position -=diceValue;
+					}
+				}
+		}
+		
+//		System.out.println(position);
 	}
 }
