b42a6467864354488316d347341d4f844810a395
jindalakhil <akhiljindal222@gmail.com>
2020-09-24 12:24:42 +0530

    [Akhil JindalAdded: actions

--- src/com/capg/SankeGame.java
+++ src/com/capg/SankeGame.java
@@ -5,5 +5,15 @@ public class SankeGame {
 		System.out.println("Welcome to game");
 		int start = 0;
 		int diceValue = (int) Math.floor(Math.random() * 10) % 6;
+		int action = (int) Math.floor(Math.random() * 10) % 3;
+		
+		int position = start;
+		if(action == 1) {
+			return;
+		}else if(action ==2) {
+			position += diceValue;
+		} else {
+			position -= diceValue;
+		}
 	}
 }
