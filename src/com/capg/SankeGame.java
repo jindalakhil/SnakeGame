7fc487c727ada02b6316e2d5cf69000e2377629d
jindalakhil <akhiljindal222@gmail.com>
2020-09-24 14:52:36 +0530

    [Akhil Jindal]Added: Player 2

--- src/com/capg/SankeGame.java
+++ src/com/capg/SankeGame.java
@@ -4,41 +4,75 @@ public class SankeGame {
 	public static void main(String[] args) {
 		System.out.println("Welcome to game");
 		int start = 0;
-		int position = start;
+		int player1 = start;
+		int player2 = start;
 		int count = 0;
 		
-		while(position != 100) {
-			int diceValue = (int) Math.floor(Math.random() * 10) % 6;
-			int action = (int) Math.floor(Math.random() * 10) % 3;
-			int noPlay = 1;
-			int ladder = 2;
-			int snake = 0;
-			count++; 
-			
-			if(action == noPlay) {
-				System.out.println("Stay in the same position");
-			}else if(action == ladder) {
-				int after = position+diceValue;
-				if(after >100) {
+		while(player1 != 100 && player2 != 100) {
+			for(int i=0; i<2;) {
+				int diceValue = (int) Math.floor(Math.random() * 10) % 6;
+				int action = (int) Math.floor(Math.random() * 10) % 3;
+				int noPlay = 1;
+				int ladder = 2;
+				int snake = 0;
+				count++; 
+				
+				if(action == noPlay) {
 					System.out.println("Stay in the same position");
-				} else {
-					position +=diceValue;
+					i++;
+				}else if(action == ladder) {
+					if(i ==1) {
+						int after = player1+diceValue;
+						if(after >100) {
+							System.out.println("Stay in the same position");
+						} else {
+							player1 +=diceValue;
+						}
+					} else {
+						int after = player2+diceValue;
+						if(after >100) {
+							System.out.println("Stay in the same position");
+						} else {
+							player2 +=diceValue;
+						}
+						
+					}	
+				} else if(action == snake){
+					
+					if(i ==1) {
+						int after = player1-diceValue;
+						if(after <0) {
+							System.out.println("moving to zero");
+							player1 = 0;
+						} else {
+							player1 -=diceValue;
+						}
+					} else {
+						int after = player2-diceValue;
+						if(after <0) {
+							System.out.println("moving to zero");
+							player2 = 0;
+						} else {
+							player2 -=diceValue;
+						}
+					}
+					
+					i++;
+					
 				}
 				
-			} else if(action == snake){
-				int after = position-diceValue;
-				if(after <0) {
-					System.out.println("moving to zero");
-					position = 0;
-				} else {
-					position -=diceValue;
-				}
+				System.out.println("player1: "+ player1 + " player2: " + player2);
 			}
 			
-			System.out.println("Updated Position: " + position);
 		}
 		
-		System.out.println("Number of time dice played: " + count);
+		if(player1 == 100 && player2 !=100) {
+			System.out.println("player 1 won");
+		}else if(player2 ==10 && player1 != 100) {
+			System.out.println("player 2 won");
+		}else if(player1 ==100 && player2 ==100) {
+			System.out.println("match draw");
+		}
 		
 	}
 }
