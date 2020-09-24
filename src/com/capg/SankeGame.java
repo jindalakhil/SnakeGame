1722c389b28eec1aa15b6c42bc6345aad46617f2
jindalakhil <akhiljindal222@gmail.com>
2020-09-24 12:20:03 +0530

    [Akhil JindalAdded: getting dice value

--- src/com/capg/SankeGame.java
+++ src/com/capg/SankeGame.java
@@ -4,5 +4,6 @@ public class SankeGame {
 	public static void main(String[] args) {
 		System.out.println("Welcome to game");
 		int start = 0;
+		int diceValue = (int) Math.floor(Math.random() * 10) % 6;
 	}
 }
