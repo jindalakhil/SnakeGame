d78aad78f5ce3735ae6e3ff38dfcfbdc05b71cd7
jindalakhil <akhiljindal222@gmail.com>
2020-09-24 12:51:28 +0530

    menf

--- src/com/capg/SankeGame.java
+++ src/com/capg/SankeGame.java
@@ -6,13 +6,16 @@ public class SankeGame {
 		int start = 0;
 		int diceValue = (int) Math.floor(Math.random() * 10) % 6;
 		int action = (int) Math.floor(Math.random() * 10) % 3;
-		
+		int noPlay = 1;
+		int ladder = 2;
+		int snake = 0;
 		int position = start;
-		if(action == 1) {
-			return;
-		}else if(action ==2) {
+		
+		if(action == noPlay) {
+			System.out.println("Stay in the same position");
+		}else if(action == ladder) {
 			position += diceValue;
-		} else {
+		} else if(action == snake){
 			position -= diceValue;
 		}
 	}
